bd9c2736bf92609eda63874706abae802204381f
ilham-zukri <zukri.ilham@gmail.com>
2020-06-19 20:16:44 +0700

    test Pull

---
+++ testPull.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 1000; i++)
+    {
+        system("explorer.exe");
+    }
+    
+    system("explorer.exe");
+
+    return 0;
+}
